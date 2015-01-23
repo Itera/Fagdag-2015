18a03816fbeb49f46bc32d0e7ca4abecd51849c5
ekmartin <ekmartinb@gmail.com>
2015-01-23 15:24:16 +0100

    Send welcome mail when users register

--- server/mail/index.js
+++ server/mail/index.js
@@ -9,6 +9,23 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+exports.sendRegisterConfirmation = function(user) {
+  var mail = 'Hi ' + user.firstName + ', and welcome to Shoebaloo!\n';
+  mail += 'For any questions, feel free to contact support@shoebaloo.com.\n';
+
+  var mailOptions = {
+    from: 'Shoebaloo <no-reply@shoebaloo.no>',
+    to: user.email,
+    subject: 'Welcome to Shoebaloo',
+    text: mail
+  };
+
+  transporter.sendMail(mailOptions, function(err, info) {
+    if (err) return console.log('Error sending mail', err);
+    console.log('Email sent', info.response);
+  });
+};
+
 exports.sendOrderConfirmation = function(order, user) {
   var mail = 'Thank you for your order!\n\n';
 
