2db670e2b39854817b0ce6f4a228b5de2b78d6fe
ekmartin <ekmartinb@gmail.com>
2015-01-23 12:00:27 +0100

    Add more user routes and controllers

--- server/routes/api/user.server.routes.js
+++ server/routes/api/user.server.routes.js
@@ -4,7 +4,14 @@ var user = require('../../controllers/user.server.controller');
 var router = express.Router();
 
 router.route('/')
-    .get(user.list)
-    .post(user.create);
+  .get(user.list)
+  .post(user.create);
+
+router.param('id', user.load);
+
+router.route('/:id')
+  .get(user.retrieve);
+
+router.get('/:id/order', user.listOrders);
 
 module.exports = router;
