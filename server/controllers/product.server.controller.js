94ebf3af7f541c9d03641ce9d402f0485a9d5fc7
ekmartin <ekmartinb@gmail.com>
2015-01-23 11:15:35 +0100

    Create Product API

--- server/controllers/product.server.controller.js
+++ server/controllers/product.server.controller.js
@@ -1,26 +1,27 @@
+var _ = require('lodash');
+var Product = require('mongoose').model('Product');
+var handleError = require('./errors.server.controller').handleError;
+
 exports.load = function(req, res, next, id) {
-  req.product = {
-    name: 'Skoen',
-    pris: 1000
-  };
-  next();
+  Product.findOne({ _id: id }, function(err, product) {
+    if (err) return handleError(res, err);
+    req.product = product;
+    next();
+  });
 };
 
 exports.create = function(req, res) {
-  res.status(201).json({
-    name: 'Sko',
-    price: 1000
+  var product = req.body;
+
+  Product.create(product, function(err, createdProduct) {
+    if (err) return handleError(res, err);
+    res.status(201).json(createdProduct);
   });
 };
 
 exports.list = function(req, res) {
-  res.status(200).json({
-    products: [
-      {
-        name: 'Sko',
-        price: 1000
-      }
-    ]
+  Product.find({}, function(err, products) {
+    res.status(200).json(products);
   });
 };
 
@@ -29,13 +30,21 @@ exports.retrieve = function(req, res) {
 };
 
 exports.delete = function(req, res) {
-  res.status(200).json({
-    message: 'Deleted product.'
+  var product = req.product;
+  product.remove(function(err) {
+    if (err) return handleError(res, err);
+    res.status(200).json({
+      message: 'Deleted product',
+      id: product.id
+    });
   });
 };
 
 exports.update = function(req, res) {
-  res.status(200).json({
-    message: 'Updated product.'
+  var updatedProduct = req.body;
+  var product = _.extend(req.product, updatedProduct);
+  product.save(function(err, updatedProduct) {
+    if (err) return handleError(res, err);
+    res.status(200).json(updatedProduct);
   });
 };
