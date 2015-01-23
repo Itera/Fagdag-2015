591c6197bc4c4b701760fd2779e5e93bb0182ae3
thomashaug <thomas.haug@itera.no>
2015-01-23 15:55:43 +0100

    initial push

---
+++ Fagdag/wwwroot/Templates/Sample/PageTypes/CommerceSamplePage.cs
@@ -0,0 +1,12 @@
+﻿using EPiServer.Core;
+
+namespace EPiServer.Commerce.Sample.Templates.Sample.PageTypes
+{
+    public abstract class CommerceSamplePage : PageData
+    {
+        public HomePage HomePage
+        {
+            get { return DataFactory.Instance.Get<HomePage>(ContentReference.StartPage); }
+        }
+    }
+}
