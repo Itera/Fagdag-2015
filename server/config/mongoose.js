8295848d3f337dcf37a637a0863ef20823ac7d09
Michele Mendel <michelemendel@gmail.com>
2015-01-23 10:52:16 +0100

    Added MongoLab's URL

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose');
 
 module.exports = function () {
-    var db = mongoose.connect('mongodb://localhost/retail', function (err) {
+
+    var mongoUrl = "mongodb://heroku_app33436366:10ugjm8gfg3mgva87kmpu30hg3@ds031571.mongolab.com:31571/heroku_app33436366";
+
+    //var db = mongoose.connect('mongodb://localhost/retail', function (err) {
+    var db = mongoose.connect(mongoUrl, function (err) {
         if (err) {
             console.log("Error connecting to database: " + err);
-        }
-        else {
+        } else {
             console.log("Connected to database");
         }
     });
